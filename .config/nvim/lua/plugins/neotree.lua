899268113c1576c03bce8d7eb2787a684ae4cc5b
Brandon Groves <brandon.groves@pm.me>
2024-08-10 15:22:52 -0400

    hide .git and node_modules

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -5,6 +5,10 @@ return {
             filtered_items = {
                 hide_dotfiles = false,
                 hide_gitignore = false,
+                hide_by_name = {
+                    ".git",
+                    "node_modules",
+                },
             },
         },
     },
